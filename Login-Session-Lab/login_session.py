44813ccde9ffcb26439a98769a65ce0ff1c0cd20
smessalem <shalevmeet@gmail.com>
2022-07-28 10:24:07 +0300

    submitting login session lab

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,22 +4,29 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['POST', 'GET'])
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+		try:
+			login_session["quote"] = request.form["quote"]
+			login_session["author"] = request.form["author"]
+			login_session["age"] = int(request.form["age"])
+			return render_template('thanks.html')
+		except:
+			return redirect(url_for('error'))
+	else:
+		return render_template('home.html')
 
 
 @app.route('/error')
 def error():
-
 	return render_template('error.html')
 
 
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', q=login_session["quote"], a=login_session["author"], age=login_session["age"])
 
 
 @app.route('/thanks')
